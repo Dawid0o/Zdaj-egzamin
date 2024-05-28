1b32b6e136448b6970a3a2f2b6bb9adddeb82630
Mateusz Lewandowski <matlewsp4@wp.pl>
2024-05-28 16:16:42 +0200

    projekt

--- Zdaj_egzaminMain.cpp
+++ Zdaj_egzaminMain.cpp
@@ -2,17 +2,20 @@
 #include <wx/textfile.h>
 #include <wx/datetime.h>
 #include <wx/textdlg.h>
+#include <wx/sound.h>
 #include <vector>
 #include <utility>
+#include <algorithm> // For std::shuffle
+#include <random>    // For std::random_device and std::mt19937
 #include "Zdaj_egzaminMain.h"
 
-// Definicje statycznych identyfikatorów
 const int Zdaj_egzaminFrame::ID_Start = wxNewId();
 const int Zdaj_egzaminFrame::ID_Authors = wxNewId();
 const int Zdaj_egzaminFrame::ID_Exit = wxNewId();
 const int Zdaj_egzaminFrame::ID_Menu = wxNewId();
 const int Zdaj_egzaminFrame::ID_Next = wxNewId();
 const int Zdaj_egzaminFrame::ID_GameHistory = wxNewId();
+const int Zdaj_egzaminFrame::ID_ToggleMusic = wxNewId(); // Add this line
 
 wxBEGIN_EVENT_TABLE(Zdaj_egzaminFrame, wxFrame)
     EVT_BUTTON(ID_Start, Zdaj_egzaminFrame::OnStart)
@@ -21,62 +24,96 @@ wxBEGIN_EVENT_TABLE(Zdaj_egzaminFrame, wxFrame)
     EVT_BUTTON(ID_Menu, Zdaj_egzaminFrame::OnReturnToMenu)
     EVT_BUTTON(ID_Next, Zdaj_egzaminFrame::OnNextQuestion)
     EVT_BUTTON(ID_GameHistory, Zdaj_egzaminFrame::OnGameHistory)
+    EVT_BUTTON(ID_ToggleMusic, Zdaj_egzaminFrame::OnToggleMusic) // Add this line
 wxEND_EVENT_TABLE()
 
 Zdaj_egzaminFrame::Zdaj_egzaminFrame(wxWindow *parent, wxWindowID id, const wxString &title, const wxPoint &pos, const wxSize &size, long style)
-    : wxFrame(parent, id, title, pos, size, style), m_currentQuestion(0), m_score(0)
+    : wxFrame(parent, id, title, pos, size, style), m_currentQuestion(0), m_score(0), m_musicPlaying(false)
 {
-    // Przykładowe pytania i odpowiedzi oraz poprawne odpowiedzi
     m_quizData.push_back(std::make_pair("Pytanie 1: Jaki jest kolor nieba?", std::vector<wxString>{"Niebieski", "Czerwony", "Zielony", "Żółty"}));
     m_quizData.push_back(std::make_pair("Pytanie 2: Ile wynosi 2 + 2?", std::vector<wxString>{"3", "4", "5", "6"}));
+    m_quizData.push_back(std::make_pair("Pytanie 3: Która planeta jest znana jako Czerwona Planeta?", std::vector<wxString>{"Mars", "Wenus", "Jowisz", "Saturn"}));
+    m_quizData.push_back(std::make_pair("Pytanie 4: Ile boków ma kwadrat?", std::vector<wxString>{"Cztery", "Trzy", "Pięć", "Sześć"}));
+    m_quizData.push_back(std::make_pair("Pytanie 5: Jakie jest największe zwierzę na świecie?", std::vector<wxString>{"Wieloryb", "Słoń", "Rekin", "Hipopotam"}));
+
     m_correctAnswers.push_back(0); // Poprawna odpowiedź dla pierwszego pytania
     m_correctAnswers.push_back(1); // Poprawna odpowiedź dla drugiego pytania
+    m_correctAnswers.push_back(0); // Poprawna odpowiedź dla trzeciego pytania
+    m_correctAnswers.push_back(0); // Poprawna odpowiedź dla czwartego pytania
+    m_correctAnswers.push_back(0); // Poprawna odpowiedź dla piątego pytania
 
     m_panel = new wxPanel(this);
     wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
 
-    m_startButton = new wxButton(m_panel, ID_Start, "Start Quiz");
-    sizer->Add(m_startButton, 0, wxALIGN_CENTER | wxALL, 5);
-
-    m_authorsButton = new wxButton(m_panel, ID_Authors, "Authors");
-    sizer->Add(m_authorsButton, 0, wxALIGN_CENTER | wxALL, 5);
-
-    m_gameHistoryButton = new wxButton(m_panel, ID_GameHistory, "Historia gry");
-    sizer->Add(m_gameHistoryButton, 0, wxALIGN_CENTER | wxALL, 5);
-    m_gameHistoryButton->Hide(); // Ukrycie przycisku na początku
-
-    m_exitButton = new wxButton(m_panel, ID_Exit, "Exit");
-    sizer->Add(m_exitButton, 0, wxALIGN_CENTER | wxALL, 5);
-
-    m_menuButton = new wxButton(m_panel, ID_Menu, "Return to Menu");
-    sizer->Add(m_menuButton, 0, wxALIGN_CENTER | wxALL, 5);
-    m_menuButton->Hide(); // Ukrycie przycisku na początku
-
-    m_nextButton = new wxButton(m_panel, ID_Next, "Next");
-    sizer->Add(m_nextButton, 0, wxALIGN_CENTER | wxALL, 5);
-    m_nextButton->Hide(); // Ukrycie przycisku na początku
-
     m_infoText = new wxStaticText(m_panel, wxID_ANY, wxT(""), wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT);
-    sizer->Add(m_infoText, 0, wxALIGN_CENTER | wxALL, 5);
+    sizer->Add(m_infoText, 0, wxALIGN_CENTER_HORIZONTAL | wxALL, 10);
+    m_infoText->Hide();
 
     m_questionText = new wxStaticText(m_panel, wxID_ANY, wxT(""), wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT);
-    sizer->Add(m_questionText, 0, wxALIGN_CENTER | wxALL, 5);
+    sizer->Add(m_questionText, 0, wxALIGN_CENTER_HORIZONTAL | wxALL, 10);
     m_questionText->Hide();
+
+    // Create a scrolled window for game history
+    m_scrolledWindow = new wxScrolledWindow(m_panel, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxVSCROLL);
+    m_scrolledWindow->SetScrollRate(5, 5);
+    wxBoxSizer* scrolledSizer = new wxBoxSizer(wxVERTICAL);
+    m_historyText = new wxStaticText(m_scrolledWindow, wxID_ANY, wxT(""), wxDefaultPosition, wxDefaultSize, wxALIGN_LEFT);
+    scrolledSizer->Add(m_historyText, 1, wxEXPAND | wxALL, 10);
+    m_scrolledWindow->SetSizer(scrolledSizer);
+    sizer->Add(m_scrolledWindow, 1, wxEXPAND | wxALL, 10);
+    m_scrolledWindow->Hide();
+
+    m_startButton = new wxButton(m_panel, ID_Start, "Start Quiz");
+    sizer->Add(m_startButton, 0, wxALIGN_CENTER_HORIZONTAL | wxALL, 10);
+
+    m_authorsButton = new wxButton(m_panel, ID_Authors, "Authors");
+    sizer->Add(m_authorsButton, 0, wxALIGN_CENTER_HORIZONTAL | wxALL, 10);
+
+    m_gameHistoryButton = new wxButton(m_panel, ID_GameHistory, "Historia gry");
+    sizer->Add(m_gameHistoryButton, 0, wxALIGN_CENTER_HORIZONTAL | wxALL, 10);
+
+    m_exitButton = new wxButton(m_panel, ID_Exit, "Exit");
+    sizer->Add(m_exitButton, 0, wxALIGN_CENTER_HORIZONTAL | wxALL, 10);
+
+    m_menuButton = new wxButton(m_panel, ID_Menu, "Return to Menu");
+    sizer->Add(m_menuButton, 0, wxALIGN_CENTER_HORIZONTAL | wxALL, 10);
+    m_menuButton->Hide();
+
+    m_nextButton = new wxButton(m_panel, ID_Next, "Next");
+    sizer->Add(m_nextButton, 0, wxALIGN_CENTER_HORIZONTAL | wxALL, 10);
+    m_nextButton->Hide();
+
+    m_toggleMusicButton = new wxButton(m_panel, ID_ToggleMusic, "Toggle Music");
+    sizer->Add(m_toggleMusicButton, 0, wxALIGN_BOTTOM | wxALL, 10);
 
     m_panel->SetSizer(sizer);
     Layout();
+
+    // Make the frame full screen
+    ShowFullScreen(true);
+
+    // Load the background music
+    m_backgroundMusic.Create("background.wav");
 }
 
 void Zdaj_egzaminFrame::OnStart(wxCommandEvent &event)
 {
     m_currentQuestion = 0;
     m_score = 0;
+
+    // Shuffle the questions
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(m_quizData.begin(), m_quizData.end(), g);
+    std::shuffle(m_correctAnswers.begin(), m_correctAnswers.end(), g);
+
     ShowQuestion(m_currentQuestion);
 
     m_startButton->Hide();
     m_authorsButton->Hide();
     m_exitButton->Hide();
     m_infoText->Hide();
+    m_gameHistoryButton->Hide();
 
     m_questionText->Show();
     m_nextButton->Show();
@@ -88,11 +125,19 @@ void Zdaj_egzaminFrame::OnAuthors(wxCommandEvent &event)
     m_startButton->Hide();
     m_authorsButton->Hide();
     m_exitButton->Hide();
+    m_gameHistoryButton->Hide();
 
-    m_menuButton->Show(true);
     m_infoText->SetLabel("Autorzy:\nJohn Doe\nJane Smith\n");
-    m_infoText->Show(); // Upewnij się, że tekst z informacjami jest widoczny
-    Layout(); // Odśwież układ elementów
+    m_infoText->Show();
+
+    wxBoxSizer* authorsSizer = new wxBoxSizer(wxVERTICAL);
+    authorsSizer->Add(m_infoText, 0, wxALIGN_LEFT | wxALL, 10);
+    authorsSizer->Add(m_menuButton, 0, wxALIGN_CENTER_HORIZONTAL | wxALL, 10);
+    m_menuButton->Show(true);
+
+    m_panel->SetSizer(authorsSizer);
+    m_panel->Layout();
+    Layout();
 }
 
 void Zdaj_egzaminFrame::OnExit(wxCommandEvent &event)
@@ -103,12 +148,15 @@ void Zdaj_egzaminFrame::OnExit(wxCommandEvent &event)
 void Zdaj_egzaminFrame::OnReturnToMenu(wxCommandEvent &event)
 {
     m_infoText->SetLabel("");
+    m_scrolledWindow->Hide();
+    m_historyText->SetLabel("");
     m_startButton->Show(true);
     m_authorsButton->Show(true);
     m_exitButton->Show(true);
+    m_gameHistoryButton->Show(true);
 
     m_questionText->Hide();
-    m_menuButton->Hide(); // Ukryj przycisk "Return to Menu" tylko jeśli przycisk "Historia gry" nie jest widoczny
+    m_menuButton->Hide();
     m_nextButton->Hide();
 
     for (auto checkbox : m_answerCheckboxes)
@@ -145,18 +193,15 @@ void Zdaj_egzaminFrame::OnNextQuestion(wxCommandEvent &event)
         wxString scoreMessage = wxString::Format("Koniec quizu! Twój wynik: %d/%d", m_score, (int)m_quizData.size());
         wxMessageBox(scoreMessage, "Wynik", wxOK | wxICON_INFORMATION);
 
-        // Pobierz podpis użytkownika
         wxString userName = wxGetTextFromUser("Podaj swoje imię lub pseudonim:", "Podpis");
         if (userName.IsEmpty())
         {
             userName = "Nieznany użytkownik";
         }
 
-        // Pobierz bieżącą datę i czas
         wxDateTime now = wxDateTime::Now();
         wxString dateTimeStr = now.FormatISOCombined(' ');
 
-        // Zapis wyniku do pliku
         wxString filePath = "wynik_quizu.txt";
         wxTextFile file;
 
@@ -174,9 +219,59 @@ void Zdaj_egzaminFrame::OnNextQuestion(wxCommandEvent &event)
         file.Write();
         file.Close();
 
-        wxCommandEvent evt; // Utworzenie tymczasowego obiektu wxCommandEvent
+        wxCommandEvent evt;
         OnReturnToMenu(evt);
     }
+}
+
+void Zdaj_egzaminFrame::OnGameHistory(wxCommandEvent& event)
+{
+    wxString filePath = "wynik_quizu.txt";
+    wxTextFile file;
+
+    if (!file.Open(filePath))
+    {
+        wxMessageBox("Nie można otworzyć pliku z historią gry.", "Błąd", wxOK | wxICON_ERROR);
+        return;
+    }
+
+    wxString history;
+    for (size_t i = file.GetLineCount(); i > 0; --i)
+    {
+        history += file.GetLine(i - 1) + "\n";
+    }
+
+    file.Close();
+
+    m_startButton->Hide();
+    m_authorsButton->Hide();
+    m_exitButton->Hide();
+    m_gameHistoryButton->Hide();
+    m_infoText->Hide();
+
+    m_historyText->SetLabel(history);
+    m_scrolledWindow->Show();
+    m_scrolledWindow->FitInside(); // Ensure scrollbar appears if needed
+    m_scrolledWindow->SetScrollRate(5, 5);  // Ensure scrollbar appears if needed
+    m_menuButton->Show();
+    m_scrolledWindow->Layout();
+    m_panel->Layout();
+    Layout();
+}
+
+void Zdaj_egzaminFrame::OnToggleMusic(wxCommandEvent &event)
+{
+    if (m_musicPlaying)
+    {
+        m_backgroundMusic.Stop();
+        m_toggleMusicButton->SetLabel("Play Music");
+    }
+    else
+    {
+        m_backgroundMusic.Play(wxSOUND_ASYNC | wxSOUND_LOOP);
+        m_toggleMusicButton->SetLabel("Stop Music");
+    }
+    m_musicPlaying = !m_musicPlaying;
 }
 
 void Zdaj_egzaminFrame::ShowQuestion(size_t questionNumber)
@@ -191,7 +286,7 @@ void Zdaj_egzaminFrame::ShowQuestion(size_t questionNumber)
             {
                 wxCheckBox *checkbox = new wxCheckBox(m_panel, wxID_ANY, "");
                 m_answerCheckboxes.push_back(checkbox);
-                m_panel->GetSizer()->Add(checkbox, 0, wxALIGN_LEFT | wxALL, 5);
+                m_panel->GetSizer()->Add(checkbox, 0, wxALIGN_CENTER_HORIZONTAL | wxALL, 5);
             }
         }
 
@@ -209,28 +304,4 @@ void Zdaj_egzaminFrame::ShowQuestion(size_t questionNumber)
 
         Layout();
     }
-}
-void Zdaj_egzaminFrame::OnGameHistory(wxCommandEvent& event)
-{
-    wxString filePath = "wynik_quizu.txt";
-    wxTextFile file;
-
-    if (!file.Open(filePath))
-    {
-        wxMessageBox("Nie można otworzyć pliku z historią gry.", "Błąd", wxOK | wxICON_ERROR);
-        return;
-    }
-
-    wxString history;
-    for (size_t i = 0; i < file.GetLineCount(); ++i)
-    {
-        history += file.GetLine(i) + "\n";
-    }
-
-    file.Close();
-
-    wxMessageBox(history, "Historia gry", wxOK | wxICON_INFORMATION);
-
-    // Po wyświetleniu historii gry pokaż przycisk powrotu do menu
-    m_menuButton->Show(true);
 }
--- Zdaj_egzaminMain.h
+++ Zdaj_egzaminMain.h
@@ -2,6 +2,7 @@
 #define ZDAJ_EGZAMINMAIN_H
 
 #include <wx/wx.h>
+#include <wx/sound.h>
 #include <vector>
 
 class Zdaj_egzaminFrame : public wxFrame
@@ -15,19 +16,24 @@ private:
     void OnExit(wxCommandEvent &event);
     void OnReturnToMenu(wxCommandEvent &event);
     void OnNextQuestion(wxCommandEvent &event);
+    void OnGameHistory(wxCommandEvent& event);
+    void OnToggleMusic(wxCommandEvent &event); // Add this line
     void ShowQuestion(size_t questionNumber);
-    int CalculateScore();
-    void OnGameHistory(wxCommandEvent& event); // Dodaj deklarację funkcji obsługi zdarzenia przycisku "Historia gry"
 
     wxButton *m_startButton;
     wxButton *m_authorsButton;
     wxButton *m_exitButton;
     wxButton *m_menuButton;
     wxButton *m_nextButton;
-    wxButton *m_gameHistoryButton; // Dodaj deklarację przycisku "Historia gry"
+    wxButton *m_gameHistoryButton;
+    wxButton *m_toggleMusicButton; // Add this line
     wxStaticText *m_infoText;
     wxStaticText *m_questionText;
+    wxStaticText *m_historyText;
+    wxScrolledWindow *m_scrolledWindow;
     wxPanel *m_panel;
+    wxSound m_backgroundMusic; // Add this line
+    bool m_musicPlaying; // Add this line
 
     size_t m_currentQuestion;
     int m_score;
@@ -35,13 +41,13 @@ private:
     std::vector<int> m_correctAnswers;
     std::vector<wxCheckBox*> m_answerCheckboxes;
 
-    // Deklaracja statycznych identyfikatorów
     static const int ID_Start;
     static const int ID_Authors;
     static const int ID_Exit;
     static const int ID_Menu;
     static const int ID_Next;
-    static const int ID_GameHistory; // Dodaj deklarację identyfikatora przycisku "Historia gry"
+    static const int ID_GameHistory;
+    static const int ID_ToggleMusic; // Add this line
 
     wxDECLARE_EVENT_TABLE();
 };
